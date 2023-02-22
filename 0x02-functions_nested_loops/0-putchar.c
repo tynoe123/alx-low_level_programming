9872861df8a949c073d0efaf1f9e3ce74ee6ae4f
tynoe <tynoe@gmail.com>
2023-02-21 22:15:20 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "putchar.h"
+#include "main.h"
 
 /**
  *   * main - Entry point
@@ -11,12 +11,10 @@ int main(void)
 			_putchar('p');
 				_putchar('u');
 					_putchar('t');
-						_putchar('c');
-							_putchar('h');
-								_putchar('a');
-									_putchar('r');
-										_putchar('\n');
+				_putchar('c');
+                                         _putchar('h');				_putchar('a');
+										_putchar('r');
+											_putchar('\n');
 
-											return (0);
+												return (0);
 }
-
