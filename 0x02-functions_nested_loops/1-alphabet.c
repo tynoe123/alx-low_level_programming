63b4d260cefdc56a2f7fe5c55e48c7a4965f07ea
tynoe <tynoe@gmail.com>
2023-02-22 00:40:50 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *   * print_alphabet - Make the alphabet
- *     *
- *       * Return: void
- *         */
+    * print_alphabet - Make the alphabet
+      *
+       * Return: void
+          */
 void print_alphabet(void)
 {
 		char c;
